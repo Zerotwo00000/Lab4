12f40b4dccd2d43abbb592780b3a3e4dc22bf79a
Zerotwo00000 <3293529428@qq.com>
2025-12-17 00:13:31 +0800

    第四次提交：实现登录控制

--- idatabase.cpp
+++ idatabase.cpp
@@ -3,13 +3,37 @@
 void IDatabase::ininDatabase()
 {
     database=QSqlDatabase::addDatabase("QSQLITE");
-    QString aFile="../../../Lab4.ab";
+    QString aFile="E:/navicat/Lab4a/Lab4.db";
     database.setDatabaseName(aFile);
 
     if(!database.open()){
         qDebug() <<"failed to open database";
     }else
-        qDebug() <<"open database is ok";
+        qDebug() <<"open database is ok" << database.connectionName();
+}
+
+QString IDatabase::userLogin(QString userName, QString password)
+{
+    QSqlQuery query;
+    query.prepare("select username,password from user where username = :USER");
+    query.bindValue(":USER",userName);
+    query.exec();
+    qDebug() << query.lastQuery() << query.first();
+
+    if(query.first() && query.value("username").isValid()){
+        QString passwd = query.value("password").toString();
+        if(passwd == password){
+            qDebug() << "login ok";
+            return "loginOK";
+        }else{
+            qDebug() << "wrong password";
+            return "wrongPassword";
+        }
+    }else{
+        qDebug() <<"no such user";
+        return "wrongUsername";
+    }
+
 }
 
 IDatabase::IDatabase(QObject *parent) : QObject{parent}
