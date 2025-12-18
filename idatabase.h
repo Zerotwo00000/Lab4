872cc211b15de894b69abb3af64c83b3a3b76b3c
Zerotwo00000 <3293529428@qq.com>
2025-12-18 13:38:55 +0800

    第五次提交：实现患者信息列表显示，查询，删除操作

--- idatabase.h
+++ idatabase.h
@@ -4,6 +4,7 @@
 #include <QObject>
 #include <QtSql>
 #include <QSqlDatabase>
+#include <QDataWidgetMapper>
 
 class IDatabase : public QObject
 {
@@ -27,7 +28,20 @@ private:
     QSqlDatabase database;
 
     void ininDatabase();
+
 signals:
+
+
+public:
+    bool initPatientModel();
+    bool searchPatient(QString filter);
+    bool deleteCurrentPatient();
+    bool submitPatientEdit();
+    void revertPatientEdit();
+
+    QSqlTableModel *patientTabModel;
+    QItemSelectionModel *thePatientSelection;
+
 };
 
 #endif // IDATABASE_H
